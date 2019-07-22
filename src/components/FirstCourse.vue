ea3ed8a61cc6bd8b57429155a74932b79b24a17b
xieyang <xieyang@wisdomgarden.com>
2019-07-22 12:48:11 +0800

    add page style

--- src/components/FirstCourse.vue
+++ src/components/FirstCourse.vue
@@ -1,9 +1,27 @@
 <template>
-    <div>
-        <h2>first course</h2>
+    <div class="tab">
+        <h2>访问分析</h2>
+        <title-tab title_num="30920" title_name="总访问量"></title-tab>
+        <title-tab title_num="1069" title_name="总访客数"></title-tab>
+        <title-tab title_num="755:27:11" title_name="总访问时长"></title-tab>
+
     </div>
 </template>
 
 <script>
-    export default {}
+    import TitleTab from './TitleTab.vue'
+
+    export default {
+        components: {
+            'title-tab': TitleTab
+        }
+    }
 </script>
+<style lang="scss">
+    .tab {
+        color: #f4f4f4;
+        width: 100%;
+        float: left;
+        position: relative;
+    }
+</style>
---
+++ src/components/TitleTab.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="tabs">
+        <div class="title_tab">
+            <span>{{title_name}}</span>
+        </div>
+        <div class="title_num">
+            {{title_num}}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['title_num', 'title_name'],
+
+    }
+</script>
+
+<style lang="scss">
+    .title_num {
+        font-size: 2.4rem;
+        color: #333;
+        margin-top: 1.2rem;
+    }
+
+    .title_tab {
+        font-size: 1.4rem;
+        margin-right: 1.2rem;
+        color: #737373;
+    }
+
+    .tabs {
+        display: inline-block;
+        width: 25%;
+        color: #737373;
+        padding: 2.1rem 2.4rem;
+        margin: 0;
+        min-width: 10rem;
+        line-height: 1;
+    }
+
+
+</style>
